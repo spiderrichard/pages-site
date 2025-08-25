f3efd2fb976e03d4d6199b9dd6edcad6041dbfeb
Richard Rowley <140534726+spiderrichard@users.noreply.github.com>
2025-08-25 14:01:13 +0100

    .

--- workers/index.js
+++ workers/index.js
@@ -1,7 +1,5 @@
 export default {
   async fetch(request) {
-    return new Response("Hello from Cloudflare Worker!", {
-      headers: { "content-type": "text/plain" }
-    });
+    return new Response("Hello from Cloudflare Worker!");
   }
 };
