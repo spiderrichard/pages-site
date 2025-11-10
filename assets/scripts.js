f8f3771519c6f09fe73fde63d6e0c042fb6ead87
Richard Rowley <richard_rowley@icloud.com>
2025-11-10 16:55:07 +0000

    .

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,1 +1,8 @@
 console.log('Script Loaded 01');
+
+const mobileMenuToggle = document.querySelector('.mobile-menu-toggle');
+const mobileMenu = document.querySelector('.menu-wrapper');
+
+mobileMenuToggle.addEventListener('click', () => {
+    mobileMenu.classList.toggle('open');
+});
