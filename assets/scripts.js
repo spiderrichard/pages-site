f905d2d5e502b8b5b683f7c196e86f8dfdf3c42a
Richard Rowley <richard_rowley@icloud.com>
2025-11-13 13:22:44 +0000

    setting up icon switch

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,14 +1,18 @@
 // Elements
+
+//nav
 const mobileMenuToggle = document.querySelector('.mobile-menu-toggle');
 const mobileMenu = document.querySelector('.menu-wrapper');
+const hamburgerMenuIcon = document.querySelector('.hamburger-menu-icon');
+const closeIcon = document.querySelector('.x-close-mark-icon');
 
 mobileMenuToggle.addEventListener('click', () => {
     mobileMenu.classList.toggle('open');
     if (mobileMenu.classList.contains('open')) {
-        console.log(mobileMenu.classList);
-        console.log('contains open');
+        hamburgerMenuIcon.classList.add('hide');
+        closeIcon.classList.remove('hide');
     } else {
-        console.log(mobileMenu.classList);
-        console.log('missing open');
+        hamburgerMenuIcon.classList.remove('hide');
+        closeIcon.classList.add('hide');
     }
 });
